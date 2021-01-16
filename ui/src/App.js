479b9a1536fdbf44f243919169ea6b0e778647fd
Waleed Rizwan <w2rizwan@edu.uwaterloo.ca>
2021-01-15 23:44:57 -0500

    added react router dom

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,50 +1,47 @@
-import './App.css';
-import  SignUp from './SignUp.js'
-import SignIn from './SignIn.js'
-import React, {Component} from 'react';
+import "./App.css";
+import SignUp from "./SignUp.js";
+import SignIn from "./SignIn.js";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 //sets initial state of web page, no image URL 0 entries
-const initialState = { 
-  route: 'SignIn',
-  isSignedIn:false,
-  user: {
-    id: '',
-    name: '',
-    email: '',       
-    verificationScore: 0,
-    joined: ''
-  
-  }
-}
-
+// const initialState = {
+//   route: "SignIn",
+//   isSignedIn: false,
+//   user: {
+//     id: "",
+//     name: "",
+//     email: "",
+//     verificationScore: 0,
+//     joined: "",
+//   },
+// };
 
 class App extends Component {
-
-  constructor () {
-    super ();
-    this.state = initialState;
+  constructor() {
+    super();
+    // this.state = initialState;
   }
 
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            
+            <Route path="/signin">
+              <SignIn />
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+            <Route path="/">
+             <div>Peepeepoopoo</div> 
+            </Route>
 
-render(){
-  return (
-    <div className="App">
-      <SignIn/>
+          </Switch>
+        </Router>
       </div>
     );
   }
-};
-
-
-
-// originial functionality
-// function App() {
-//   return (
-//     <div className="App">   
-//     <SignIn/>
-
-//     </div>
-//   );
-// }
-
-export default App;
+} export default App;
