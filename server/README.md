b9c80ecadc52f022e0c79229381829aa614b80ab
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2021-01-15 23:25:10 -0500

    add docker setup

---
+++ server/README.md
@@ -0,0 +1,7 @@
+# Getting started
+
+### Development:
+
+1. Download Docker
+2. `cd server && docker-compose up`
+3. ``
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@material-ui/core";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button color="primary">Hello World</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
